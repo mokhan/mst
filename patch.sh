9bf9c88038a8fdb5da99e9ccbeedcb0205ccd32a
mo khan <mo.khan@gmail.com>
2019-12-20 19:58:28 -0700

    Add font2 patch

--- patch.sh
+++ patch.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+wget https://st.suckless.org/patches/font2/st-font2-20190416-ba72400.diff
 wget https://st.suckless.org/patches/boxdraw/st-boxdraw_v2-0.8.2.diff
 wget https://st.suckless.org/patches/newterm/st-newterm-0.8.2.diff
 wget https://st.suckless.org/patches/nordtheme/st-nordtheme-0.8.2.diff
 wget https://st.suckless.org/patches/desktopentry/st-desktopentry-0.8.2.diff
 wget https://st.suckless.org/patches/clipboard/st-clipboard-20180309-c5ba9c0.diff
+
 git apply ./*.diff
 rm ./*.diff
