bec00c5092cf7d2f95d3964065decfb2b7171e1a
mo khan <mo.khan@gmail.com>
2019-12-20 20:38:06 -0700

    Apply patch to start st via tmux

--- patch.sh
+++ patch.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 apply_patch() {
   url=$1
   wget "$url"
@@ -14,4 +16,4 @@ apply_patch https://st.suckless.org/patches/newterm/st-newterm-0.8.2.diff
 apply_patch https://st.suckless.org/patches/nordtheme/st-nordtheme-0.8.2.diff
 apply_patch https://st.suckless.org/patches/desktopentry/st-desktopentry-0.8.2.diff
 apply_patch https://st.suckless.org/patches/clipboard/st-clipboard-20180309-c5ba9c0.diff
-#apply_patch https://gist.githubusercontent.com/mokhan/62bba469937d0114778af4e08ab5f53b/raw/f386b4f738585e90564e457c9795a681333b54e8/st-exec-tmux.diff
+apply_patch https://gist.githubusercontent.com/mokhan/62bba469937d0114778af4e08ab5f53b/raw/f386b4f738585e90564e457c9795a681333b54e8/st-exec-tmux.diff
